6361a5d28b09dec8e8b10299308ee09ec92cbedc
Rajiv Mirpuri <rajiv@Rajivs-MacBook-Pro.local>
2017-03-30 15:27:26 -0400

    start selenium server script

---
+++ startSeleniumServer.sh
@@ -0,0 +1,7 @@
+pwd
+cd /
+# start selenium
+/usr/local/lib/node_modules/protractor/bin/webdriver-manager start
+
+# wait until selenium is up
+#while ! curl http://localhost:4444/wd/hub/status &>/dev/null; do :; done
